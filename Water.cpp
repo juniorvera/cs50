80efd70faa6946fe01ec7b11704eef4ce958d6f2
Vera <verastepanoova@gmail.com>
2019-03-04 23:56:13 +0300

    Add files via upload

---
+++ Water.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+
+int main()
+{
+	const int litersInMinute = 6;
+	int time, liters;
+    std::cout <<"Hello!\n How long have you shower? Input minutes: ";
+	std::cin >> time;
+	if (time <= 0) 
+	{
+		do 
+		{
+			std::cout << "Retry: ";
+			std::cin >> time;
+		} while (time <= 0);
+	}
+	else
+	{
+		liters = time * litersInMinute;
+		std::cout << "You pour out "<<liters<<" liters, when you are in the shower.";
+	}
+
+}
+
